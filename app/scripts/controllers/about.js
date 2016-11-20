73ecee897127f7880f4be5030f214d0904515770
pedro misael canche angulo <pedro.canche@uimqroo.edu.mx>
2016-11-20 11:12:49 -0600

    Se inicio el proyecto

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name menuApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the menuApp
+ */
+angular.module('menuApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name menuApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the menuApp
+ */
+angular.module('menuApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
